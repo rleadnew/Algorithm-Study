7cdd4b3e11532bab7f79ec532c60facaf703e133
JW <87660628+rleadnew@users.noreply.github.com>
2021-12-16 00:41:56 +0900

    Update Binary_search.py

--- Binary-Search/Binary_search.py
+++ Binary-Search/Binary_search.py
@@ -6,7 +6,7 @@ def binary_search(array, target, start, end):
     # 찾은 경우 중간점 인덱스 반환
     if array[mid] == target:
         return mid
-    # 중간점의 값보다 찾고자 한느 값이 작은 경우 왼쪽 확인
+    # 중간점의 값보다 찾고자 한는 값이 작은 경우 왼쪽 확인
     elif array[mid] > target:
         return binary_search(array, target, start, mid - 1)
     else:
