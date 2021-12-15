4b3cafd0a9d51d0bbcc54545c0a589be53e1eb02
JW <87660628+rleadnew@users.noreply.github.com>
2021-12-15 22:20:15 +0900

    crane

---
+++ Programmers-PS/crane-ps-01.py
@@ -0,0 +1,27 @@
+def solution(board, moves):
+    answer = 0
+    finalList = []
+
+    for move in moves:
+        for i in range(len(board)):
+            if board[i][move - 1] != 0:
+                finalList.append(board[i][move - 1])
+                board[i][move - 1] = 0
+
+                if len(finalList) > 1:
+                    if finalList[-1] == finalList[-2]:
+                        finalList.pop(-1)
+                        finalList.pop(-1)
+                        answer += 2
+                break
+
+    return answer
+
+test1 = solution([[0,0,0,0,0],
+                  [0,0,1,0,3],
+                  [0,2,5,0,1],
+                  [4,2,4,4,2],
+                  [3,5,1,3,1]], [1,5,3,5,1,2,1,4])
+
+print(test1)
+
